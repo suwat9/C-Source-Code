7b384a9b09d2f66b766424bb2a876760c82ec748
Suwat <suwat.tac@mail.pbru.ac.th>
2025-07-15 11:20:10 +0700

    Create Lab2-Input.cpp

---
+++ Lab2-Input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main() {
+	int x;
+	cout << "Hello world ....";
+	cout << endl << "Input a number : ";
+	cin >> x ;
+	cout << "You input x = " << x << endl;
+}
