ee69f1b18dbdf882bd5973a056582004517106d7
Suwat <suwat.tac@mail.pbru.ac.th>
2025-07-29 11:27:38 +0700

    Update lab20-triangle.hpp

--- lab20-triangle.hpp
+++ lab20-triangle.hpp
@@ -21,3 +21,19 @@ class Triangle {
 		std::cout << "Area of triangle = " << area << std::endl;		
 	}
 };
+
+class Rectangle {
+	public:
+		int width, length;
+	
+	void setRectangle(int w, int l){
+		width = w;
+		length = l;
+	}
+	void perimeter(){
+		std::cout << "Perimeter of rectangle is " << (2*(width+length))<< std::endl;
+	}
+	void area(){
+		std::cout << "Area of rectangle is " << (width*length) << std::endl;
+	}
+};
